a63d4f79c7a5f957dd306290ba771ffc129f28a7
Marc A. Mauger <simianarmy@gmail.com>
2011-11-06 11:24:03 -0800

    Minor updates to make it work

--- app.rb
+++ app.rb
@@ -4,24 +4,26 @@
 require 'csv'
 require 'fileutils'
 
-csv_file = File.join(File.dirname(__FILE__), 'resources', 'nfl-teams.csv') #<---- modify this file for the teams
+TEAM_CSV  = $ARGV[0]
+IN_DIR    = '/Users/marcmauger/Desktop/PushCerts/' 
+SSH_USER  = 'deploy'
 
-IN_DIR = '/Users/jprichardson/Desktop/PushCerts/'
-
-Dir.mkdir(OUT_DIR) unless Dir.exists?(OUT_DIR)
-
+unless TEAM_CSV && File.exists?(TEAM_CSV)
+  puts "Usage: app.rb team_csv_file"
+  exit
+end
 files = Dir.entries(IN_DIR).find_all{|f| !f.start_with?('.') }
 
-CSV.foreach(csv_file, headers: :frontrow) do |row|
+CSV.foreach(TEAM_CSV, {headers: :first_row}) do |row|
   if row['bundle_id'] != ''
     is_match = false
-    out_loc = "root@data.mobilefanapps.com:/var/www/current/keys/#{row['id']}.pem"
+    out_loc = "#{SSH_USER}@data.mobilefanapps.com:/var/apns/keys/#{row['id']}.pem"
     files.each do |file|
       in_file = IN_DIR + file
       if file.include?(row['bundle_id'])
         puts("Copying #{in_file} to #{out_loc}...")
         is_match = true
-        `scp -P 59999 #{in_file} root@data.mobilefanapps.com:/var/www/current/keys/.`
+        `scp -P 59999 #{in_file} #{out_loc}`
         break;
       end
     end
